4bf17d980c5942820524060bfc8d548349772aa6
arfiansr <srarfian@gmail.com>
2025-09-03 15:30:35 +0700

    fix: change userId type to UUID

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,9 +1,13 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type User struct {
-	ID        int       `json:"id" db:"id"`
+	ID        uuid.UUID `json:"id" db:"id"`
 	Name      string    `json:"name" db:"name"`
 	Email     string    `json:"email" db:"email"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
